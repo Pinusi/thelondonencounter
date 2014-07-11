92e49de17d41d116503070d109a9131a5f8a6bd2
Cecilia De Conto <cecilia.de.conto@pulselive.com>
2014-07-11 01:21:08 +0100

    updates

--- IN/scripts/scripts.js
+++ IN/scripts/scripts.js
@@ -1,22 +1,22 @@
 function initialize () {
 	$(document).foundation();
-	$("#programme_link").bind("click", function() {
+	$(".programme_link").bind("click", function() {
 		$(window).scrollTop($("#programme").offset().top); 
 		return false; //Prevent Default and event bubbling.
 	});
-	$("#talks_link").bind("click", function() {
+	$(".talks_link").bind("click", function() {
 		$(window).scrollTop($("#talks").offset().top); 
 		return false; //Prevent Default and event bubbling.
 	});
-	$("#exibitions_link").bind("click", function() {
+	$(".exibitions_link").bind("click", function() {
 		$(window).scrollTop($("#exibitions").offset().top); 
 		return false; //Prevent Default and event bubbling.
 	});
-	$("#location_link").bind("click", function() {
+	$(".location_link").bind("click", function() {
 		$(window).scrollTop($("#location").offset().top); 
 		return false; //Prevent Default and event bubbling.
 	});
-	$("#sponsors_link").bind("click", function() {
+	$(".sponsors_link").bind("click", function() {
 		$(window).scrollTop($("#sponsors").offset().top); 
 		return false; //Prevent Default and event bubbling.
 	});
