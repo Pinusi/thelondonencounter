8c3d71f5b6f84df306e1dfd88e67a533464c1dee
Cecilia De Conto <cecilia.de.conto@pulselive.com>
2014-07-12 12:40:32 +0100

    Mobile menu + Programme fro desktop

--- IN/scripts/scripts.js
+++ IN/scripts/scripts.js
@@ -1,27 +1,42 @@
 function initialize () {
 	$(document).foundation();
-	$(".programme_link").bind("click", function() {
-		$(window).scrollTop($("#programme").offset().top); 
-		return false; //Prevent Default and event bubbling.
-	});
-	$(".talks_link").bind("click", function() {
-		$(window).scrollTop($("#talks").offset().top); 
-		return false; //Prevent Default and event bubbling.
-	});
-	$(".exibitions_link").bind("click", function() {
-		$(window).scrollTop($("#exibitions").offset().top); 
-		return false; //Prevent Default and event bubbling.
-	});
-	$(".location_link").bind("click", function() {
-		$(window).scrollTop($("#location").offset().top); 
-		return false; //Prevent Default and event bubbling.
-	});
-	$(".sponsors_link").bind("click", function() {
-		$(window).scrollTop($("#sponsors").offset().top); 
-		return false; //Prevent Default and event bubbling.
-	});
+
+	attachClick( ".programme_link" , "#programme" );
+	attachClick( ".talks_link" , "#talks" );
+	attachClick( ".exibitions_link" , "#exibitions" );
+	attachClick( ".location_link" , "#location" );
+	attachClick( ".sponsors_link" , "#sponsors" );
 
 	$(".sandwich").bind("click", function() {
 		$(".header_dropdown").toggle();
+		return false; //Prevent Default and event bubbling.
+	});
+
+	$(".close").bind("click", function() {
+		$(".header_dropdown").toggle();
+		return false; //Prevent Default and event bubbling.
+	});
+}
+
+function attachClick( source_class, target_id ){
+	$( source_class ).bind("click", function() 
+	{
+		$(window).scrollTop($( target_id ).offset().top);
+
+		if($(".header_dropdown").is(":visible") ){
+			$(".header_dropdown").toggle();
+		}
+
+		return false; //Prevent Default and event bubbling.
+	});
+
+	$( source_class ).mousedown(function()
+	{
+		$( this ).find('a').addClass('click');
+	});
+		
+	$( source_class ).mouseup(function()
+	{
+		$( this ).find('a').removeClass('click');
 	});
 }
